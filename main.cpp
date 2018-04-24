579dfe25206271ef3f4b0e3f6822e4035322f700
Felicia <38441708+felicia-pastrano@users.noreply.github.com>
2018-04-24 15:35:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -75,7 +75,7 @@ int main()
 	while (exit != true)
 	{
 		cout << endl << "Press 0 to Open Menu" << endl;
-		cout << "Press 4 to Exit" << endl;
+		cout << "Press 5 to Exit" << endl;
 		cout << "Your Choice: ";
 		cin >> menu;
 		if (menu == 0) //IF USER DECIDES TO OPEN MENU
@@ -83,7 +83,7 @@ int main()
 			calcmenu.menuprint();	//PRINT THE MENU
 			calcmenu.choices();
 		}
-		else if (menu == 4) // TO EXIT FROM FIRST SCREEN WITH OPTION FOR MENU OR EXIT
+		else if (menu == 5) // TO EXIT FROM FIRST SCREEN WITH OPTION FOR MENU OR EXIT
 		{
 			exit = true;
 		}
